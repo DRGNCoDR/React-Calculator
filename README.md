94d9aa2635a5512274a33d71a6794f3af65daa63
Ray <rayfradella@yahoo.com>
2023-10-17 00:34:06 -0400

    v.0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # React-Calculator
+
+#React-Calculator
+> A basic react calculator built in react
+>
+>
+
+** v0.0.1 **
+
+- [x] add, minus, divide, multiply, backspace, clear, percent functionality
+- [x] Add basic layout
+- [x] basic calculator funtional
+- [x] external css file
+- [ ] Styling
+- [ ] Reactify Code
+- [ ] Check you can only add one . and (-)
+- [ ] make (-) toggle negative instead of just adding it
+- [ ] look into incorporating ( ) functionality into the input
--- src/comp/app.js
+++ src/comp/app.js
@@ -1,90 +1,246 @@
 import React from "react";
 
-function App(){
-  return (
-    <div>
-        <h2 className="textCenter">React Calculator</h2>
-        <div
-            className="calc-container"
-        >        
+function App()
+{
+    let firstNum = 0,
+    secondNum = 0,
+    operator = ""
+
+    const handleBackButton = () => {
+        let inputVal = document.querySelector(".calc-input").value
+        
+        if(inputVal != "")
+        {
+            document.querySelector(".calc-input").value = inputVal.slice(0,-1)
+        }
+    }
+
+    const handleClearButton = () => {
+        document.querySelector(".calc-input").value = ""
+    }
+
+    const handlePercentButton = () => {
+        let inputVal = document.querySelector(".calc-input").value
+        if (inputVal != "")
+        {
+            document.querySelector(".calc-input").value = (inputVal / 100)
+        }
+    }
+
+    let addToInput = (key) =>
+    {
+        let inputVal = document.querySelector(".calc-input").value
+            
+        if (key != "") 
+        {
+            switch (key) 
+            {
+                case '0': 
+                    document.querySelector(".calc-input").value = 
+                    inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '1': 
+                    document.querySelector(".calc-input").value = inputVal + key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '2': 
+                    document.querySelector(".calc-input").value = inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '3': 
+                    document.querySelector(".calc-input").value = inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '4': 
+                    document.querySelector(".calc-input").value = inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '5': 
+                    document.querySelector(".calc-input").value = inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '6': 
+                    document.querySelector(".calc-input").value = inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '7': 
+                    document.querySelector(".calc-input").value = inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '8': 
+                    document.querySelector(".calc-input").value = inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '9': 
+                    document.querySelector(".calc-input").value = inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '.': 
+                    document.querySelector(".calc-input").value = inputVal+ key.toString()
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '(-)':
+                    document.querySelector(".calc-input").value = "-" + inputVal
+                    inputVal = document.querySelector(".calc-input").value
+                break;
+
+                case '+': 
+                    firstNum = inputVal
+                    operator = "+"
+                    handleClearButton()
+                break;
+
+                case '-': firstNum = inputVal
+                    operator = "-"
+                    handleClearButton()
+                break;
+
+                case '*': firstNum = inputVal
+                    operator = "*"
+                    handleClearButton()
+                break;
+
+                case '/': firstNum = inputVal
+                    operator = "/"
+                    handleClearButton()
+                break;
+
+                case '=':
+                    secondNum = document.querySelector(".calc-input").value
+                    
+                    if(operator == "+")
+                    {
+                        inputVal = (parseInt(firstNum) + parseInt(secondNum))
+                        document.querySelector(".calc-input").value = inputVal
+                        console.log(inputVal)
+                    }
+
+                    if(operator == "-")
+                    {
+                        inputVal = (parseInt(firstNum) - parseInt(secondNum))
+                        document.querySelector(".calc-input").value = inputVal
+                    }
+
+                    if(operator == "/")
+                    {
+                        inputVal = (parseInt(firstNum) / parseInt(secondNum))
+                        document.querySelector(".calc-input").value = inputVal
+                    }
+
+                    if(operator == "*")
+                    {
+                        inputVal = (parseInt(firstNum) * parseInt(secondNum))
+                        document.querySelector(".calc-input").value = inputVal
+                    }
+                break;
+            }
+        }
+    }
+    return (
+        <div>
+            <h2 className="textCenter">React Calculator</h2>
+            
             <div
-                className="calcBtnRow"
-            >
-                <input className=""/>
-                <button className="calcBtn">
-                    Back
-                </button>
-            </div>        
-            <div className="calcBtnRow">
-                <button className="calcBtn">
-                    C    
-                </button>
-                <button className="calcBtn">
-                    C/E
-                </button>
-                <button className="calcBtn">
-                    %
-                </button>                
+                className="calc-container"
+            >        
+                <div
+                    className="calcBtnRow"
+                >
+                    <input className="calc-input"/>
+                    <button className="calcBtn"  onClick={handleBackButton}>
+                        Back
+                    </button>
+                </div>  
+
+                <div className="calcBtnRow">
+                    <button className="calcBtn"  onClick={handleClearButton}>
+                        C    
+                    </button>
+                    <button className="calcBtn"  >
+                        C/E
+                    </button>
+                    <button className="calcBtn"  onClick={handlePercentButton}>
+                        %
+                    </button>  
+                    <button className="calcBtn" onClick={() => addToInput("-")}>
+                        -
+                    </button>              
+                </div>
+
+                <div className="calcBtnRow">
+                    <button className="calcBtn" onClick={() => addToInput("7")}>
+                        7    
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("8")} >
+                        8
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("9")}>
+                        9
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("/")}>
+                        /
+                    </button>
+                </div>
+
+                <div className="calcBtnRow">
+                    <button className="calcBtn" onClick={() => addToInput("4")}>
+                        4    
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("5")}>
+                        5
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("6")}>
+                        6
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("*")}>
+                        *
+                    </button>
+                </div>
+
+                <div className="calcBtnRow">
+                    <button className="calcBtn" onClick={() => addToInput("1")}>
+                        1    
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("2")}>
+                        2
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("3")}>
+                        3
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("+")}>
+                        +
+                    </button>
+                </div>
+
+                <div className="calcBtnRow">
+                    <button className="calcBtn" onClick={() => addToInput("(-)")}>
+                        ( - )    
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("0")}>
+                        0
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput(".")}>
+                        .
+                    </button>
+                    <button className="calcBtn" onClick={() => addToInput("=")}>
+                        =
+                    </button>   
+                </div>
             </div>
-            <div className="calcBtnRow">
-                <button className="calcBtn">
-                    7    
-                </button>
-                <button className="calcBtn">
-                    8
-                </button>
-                <button className="calcBtn">
-                    9
-                </button>
-                <button className="calcBtn">
-                    /
-                </button>
-            </div>
-            <div className="calcBtnRow">
-                <button className="calcBtn">
-                    4    
-                </button>
-                <button className="calcBtn">
-                    5
-                </button>
-                <button className="calcBtn">
-                    6
-                </button>
-                <button className="calcBtn">
-                    *
-                </button>
-            </div>
-            <div className="calcBtnRow">
-                <button className="calcBtn">
-                    1    
-                </button>
-                <button className="calcBtn">
-                    2
-                </button>
-                <button className="calcBtn">
-                    3
-                </button>
-                <button className="calcBtn">
-                    +
-                </button>
-            </div>
-            <div className="calcBtnRow">
-                <button className="calcBtn">
-                    ( - )    
-                </button>
-                <button className="calcBtn">
-                    0
-                </button>
-                <button className="calcBtn">
-                    .
-                </button>
-                <button className="calcBtn">
-                    =
-                </button>   
-            </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
